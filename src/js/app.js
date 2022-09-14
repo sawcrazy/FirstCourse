45ed431e763e20c1ebbb7016f89d324f7e608ffe
sawcrazy <sawvrazy@yandex.ru>
2022-09-14 15:45:32 +0300

    работа с JS

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,3 @@
-let site = prompt('Выберете тип сайта. Нужно поставить цифру от 1 до 3, где 1 - Сайт-визитка; 2 - Корпоративный сайт; 3 - Интернет-магазин.');
-let desing = prompt('Выберете дизайн. Нужно поставить цифру от 1 до 3, где 1 - Шаблонный; 2 - Уникальный; 3 - WOW-Дизайн.');
-let adaptability = prompt('Выберете адаптивность. Нужно поставить цифру от 1 до 3, где 1 - Только ПК; 2 - Только мобильный сайт; 3 - ПК + Мобильный.');
 
 const sitePrice ={
     1:5000,
@@ -17,18 +14,38 @@ const adaptabilityPrice ={
     2:10000,
     3:15000,
 }
+const siteDays ={
+    1:2,
+    2:2,
+    3:2,
+}
+const desingDays ={
+    1:2,
+    2:3,
+    3:4,
+}
+const adaptabilityDays ={
+    1:1,
+    2:3,
+    3:5,
+}
 
-function summa() {
+function getSummaDays() {
+    let site = prompt('Выберете тип сайта. Нужно поставить цифру от 1 до 3, где 1 - Сайт-визитка; 2 - Корпоративный сайт; 3 - Интернет-магазин.');
+    let desing = prompt('Выберете дизайн. Нужно поставить цифру от 1 до 3, где 1 - Шаблонный; 2 - Уникальный; 3 - WOW-Дизайн.');
+    let adaptability = prompt('Выберете адаптивность. Нужно поставить цифру от 1 до 3, где 1 - Только ПК; 2 - Только мобильный сайт; 3 - ПК + Мобильный.');
     if(site in sitePrice && desing in desingPrice && adaptability in adaptabilityPrice) {
         const sum = sitePrice[site] + desingPrice[desing] + adaptabilityPrice[adaptability]
-        const textPrice = `Сумма вашего заказа = ${sum} рублей`
-        return textPrice
+        const days = siteDays[site] + desingDays[desing] + adaptabilityDays[adaptability]
+        const textPriceDays = `Сумма вашего заказа = ${sum} рублей. Выполнение заказа займет  ${days} дней`
+        return alert(textPriceDays)
     }else {
         const textError = 'Неверный ввод данных'
-        return textError
+        alert(textError)
+        getSummaDays()
     }
 
 }
-alert(summa())
+getSummaDays()
 
 
